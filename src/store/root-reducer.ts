8c2907050eead8cb5ab67aba3a6b5045d2bd38e0
Andrey Kosov < emailAndrejcosov@yandex.ru>
2022-06-08 18:33:42 +0300

    Add form serch

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import {productsReducer} from './products/products-reducer';
+import {searchReducer} from './search/search-reducer';
 import {Namespace} from '../constants';
 
 const rootReducer = combineReducers({
   [Namespace.Products]: productsReducer,
+  [Namespace.Search]: searchReducer,
 });
 
 export {rootReducer};
